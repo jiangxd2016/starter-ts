82fd13fd0aeb23f1a2ccef7cd64d15cda3cd29d3
jiangxd <jiangxd2016@gmail.com>
2023-03-14 14:38:07 +0800

    fix: fix test error

--- playground/vitest.config.ts
+++ playground/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config';
-
+import Vue from '@vitejs/plugin-vue';
+import Jsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
+  plugins: [Vue(), Jsx()],
   test: {
     environment: 'jsdom',
     globals: true,
